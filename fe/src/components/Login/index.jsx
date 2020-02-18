b9b89552fca5c1aae244d0654c42d29103c3a9a6
eduardpapiian <mikroffon@gmail.com>
2020-02-18 17:38:21 +0200

    added react-dom-router

--- fe/src/components/Login/index.jsx
+++ fe/src/components/Login/index.jsx
@@ -5,13 +5,14 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 
 function Copyright() {
   return (
@@ -102,16 +103,16 @@ export default function SignIn() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href="/register" variant="body2">
+              <Link to="/register" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+      {/*<Box mt={8}>*/}
+        {/*<Copyright />*/}
+      {/*</Box>*/}
     </Container>
   );
 }
